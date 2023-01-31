b11da48704b4b3f87b3b3cde3464668a049d6d55
juancgalueweb <juancgalue@icloud.com>
2023-01-31 01:20:29 -0300

    Remove unnecessary await

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,12 +7,12 @@ const LOCAL_HOST_URL = 'http://localhost:5173/'
 test('app shows random facts and a single image', async ({ page }) => {
   await page.goto(LOCAL_HOST_URL)
 
-  const text = await page.getByRole('paragraph')
-  const image = await page.getByRole('img')
+  const text = page.getByRole('paragraph')
+  const image = page.getByRole('img')
 
   const textContext = await text.textContent()
   const imageSrc = await image.getAttribute('src')
 
-  await expect(textContext?.length).toBeGreaterThan(0)
-  await expect(imageSrc?.startsWith(CAT_PREFIX_IMAGE_URL)).toBeTruthy()
+  expect(textContext?.length).toBeGreaterThan(0)
+  expect(imageSrc?.startsWith(CAT_PREFIX_IMAGE_URL)).toBeTruthy()
 })
